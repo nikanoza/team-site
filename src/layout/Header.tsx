e6b09b607a84775a85934c535e6c8312c6c54f04
Nika Nozadze <nikanozadze1010@gmail.com>
2023-09-21 17:52:03 +0400

    feat: create header component

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,5 +1,12 @@
+import { Hamburger, Logo } from "../svg";
+
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className="w-full flex justify-between items-center pt-12 px-6">
+      <Logo />
+      <Hamburger onClick={() => {}} />
+    </header>
+  );
 };
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        smoke: "#014E56",
+      },
+    },
   },
   plugins: [],
 };
