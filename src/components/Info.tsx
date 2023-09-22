12ad03cb24838905cc6c25adaf878ffc9a7338b2
Nika Nozadze <nikanozadze1010@gmail.com>
2023-09-22 18:52:52 +0400

    feat: create info reusable component

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,5 +1,19 @@
-const Info = () => {
-  return <div></div>;
+type PropsType = {
+  image: React.ReactNode;
+  title: string;
+  description: string;
+};
+
+const Info: React.FC<PropsType> = ({ image, title, description }) => {
+  return (
+    <div className="flex flex-col items-center">
+      {image}
+      <h3 className="mt-4 text-red text-lg font-bold">{title}</h3>
+      <h4 className="text-white text-[15px] leading-6 font-semibold opacity-80">
+        {description}
+      </h4>
+    </div>
+  );
 };
 
 export default Info;
